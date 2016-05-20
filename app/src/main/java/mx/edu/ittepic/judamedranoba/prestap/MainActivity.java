16f499b3c794286407bde2e642a48f5bc6452cb8
eabo94 <edanbarbosaor@ittepic.edu.mx>
2016-05-20 20:57:32 +0200

    actualizacion

--- app/src/main/java/mx/edu/ittepic/judamedranoba/prestap/MainActivity.java
+++ app/src/main/java/mx/edu/ittepic/judamedranoba/prestap/MainActivity.java
@@ -2,6 +2,12 @@ package mx.edu.ittepic.judamedranoba.prestap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final DatePicker dp =(DatePicker)findViewById(R.id.dp);
+        final Button setBtn=(Button)findViewById(R.id.set);
+
+        setBtn.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this,dp.getDayOfMonth()+ " " + dp.getMonth() + " " + dp.getYear(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
